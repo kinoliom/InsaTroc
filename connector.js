837c1f5d8cec1273dc58f0d85b85f0615d5d0332
Noëline <noeline.migeon@gmail.com>
2020-04-03 10:18:18 +0200

    tuto_express

--- connector.js
+++ connector.js
@@ -1,5 +1,21 @@
-const mariadb = require('mariadb');
+//const mariadb = require('mariadb');
+var mysql = require('mysql');
 
+var connection = mysql.createConnection({
+	host:'localhost',
+	user:'dbuser',
+	database:'my_db'
+})
+
+connection.connect();
+connection.query('SELECT 1 + 1 AS solution', function(err,rows,fields){
+	if(err) throw err;
+	console.log('The solution is: ',rows[0].solution)
+})
+
+connection.end();
+
+/*
 mariadb.createConnection({
 	user:"root",
 	database:"db",
@@ -14,4 +30,4 @@ mariadb.createConnection({
 		console.log("Error: " + err.message);
 	});
 
-
+*/
